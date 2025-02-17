0faab927db47f18e2ea89afd9727f1ced187fdbb
simonrydder <66967123+simonrydder@users.noreply.github.com>
2025-02-17 14:52:48 +0100

    updated main to run with resnet model

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ from src.concrete.standard_data_creater import StandardDataCreater
 from src.concrete.standard_model_loader import StandardModelLoader
 from src.concrete.standard_model_trainer import StandardModelTrainer
 from src.concrete.standard_model_updater import StandardModelUpdater
-from src.models.simple import Simple
+from src.data.resnet.simple_images import SimpleImages
+from src.models.resnet import Resnet
 
 
 def fine_tune_model():
-    loader = StandardModelLoader(Simple)
-    old_model = loader.load_pretrained("data/ckpt/simple_linear_one.ckpt")
+    # loader = StandardModelLoader(Simple)
+    loader = StandardModelLoader(Resnet)
+    # old_model = loader.load_pretrained("data/ckpt/simple_linear_one.ckpt")
+    old_model = loader.load_pretrained("data/ckpt/resnet_pretrained.ckpt")
 
     updater = StandardModelUpdater()
     new_model = updater.adapt(old_model)
 
-    data_creater = StandardDataCreater()
+    data_creater = StandardDataCreater(SimpleImages(1000, 10, distribution="shifted"))
     train = data_creater.create_training_loader()
     val = data_creater.create_validation_loader()
     test = data_creater.create_test_loader()
@@ -24,8 +27,8 @@ def fine_tune_model():
     trainer.fit(new_model, train, val)
     trainer.test(new_model, test)
 
-    print("Pretrained:", old_model.state_dict())
-    print("Fine Tunes:", new_model.state_dict())
+    # print("Pretrained:", old_model.state_dict())
+    # print("Fine Tunes:", new_model.state_dict())
 
 
 if __name__ == "__main__":

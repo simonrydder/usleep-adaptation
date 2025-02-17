216362180bcefbccfb49b127de9368bb0c58d43b
simonrydder <66967123+simonrydder@users.noreply.github.com>
2025-02-17 18:50:52 +0100

    added adapter to model updater

--- src/interfaces/model_updater.py
+++ src/interfaces/model_updater.py
@@ -2,9 +2,11 @@ from abc import ABC, abstractmethod
 
 from lightning import LightningModule
 
+from src.interfaces.adapter import Adapter
+
 
 class ModelUpdater(ABC):
-    def __init__(self) -> None:
+    def __init__(self, adapter: type[Adapter]) -> None:
         super().__init__()
 
     @abstractmethod

6d432291d899c76be889028828c445b1c138ef8b
simonrydder <66967123+simonrydder@users.noreply.github.com>
2025-02-17 18:49:42 +0100

    added type hit on registries

--- src/config/_registries.py
+++ src/config/_registries.py
@@ -1,11 +1,14 @@
+from lightning import LightningModule
+
 from src.concrete.adapters.bitfit import BitFit
+from src.interfaces.adapter import Adapter
 from src.models.resnet import Resnet
 from src.models.simple import Simple
 
-MODEL_REGISTRY = {
+MODEL_REGISTRY: dict[str, type[LightningModule]] = {
     "simple": Simple,
     "resnet": Resnet,
 }
-ADAPTER_REGISTRY = {
+ADAPTER_REGISTRY: dict[str, type[Adapter]] = {
     "bitfit": BitFit,
 }

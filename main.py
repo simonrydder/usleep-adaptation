f856e0176403be7fb0e30c3bbb0c70e3d10299da
simonrydder <66967123+simonrydder@users.noreply.github.com>
2025-02-17 18:51:56 +0100

    model updater with config

--- main.py
+++ main.py
@@ -3,19 +3,23 @@ from src.concrete.standard_model_loader import StandardModelLoader
 from src.concrete.standard_model_trainer import StandardModelTrainer
 from src.concrete.standard_model_updater import StandardModelUpdater
 from src.config.config import load_config
-from src.data.resnet.simple_images import SimpleImages
+from src.dataset.resnet.simple_images import SimpleImages
+from src.dataset.simple.simple_linear import SimpleLinear
 
 
 def fine_tune_model():
     config = load_config("simple_bitfit")
 
-    loader = StandardModelLoader(config.get_model_class())
+    model_cls = config.get_model_class()
+    loader = StandardModelLoader(model_cls)
     old_model = loader.load_pretrained(config.ckpt)
 
-    updater = StandardModelUpdater()
+    adapter = config.get_adapter()
+    updater = StandardModelUpdater(adapter)
     new_model = updater.adapt(old_model)
 
     data_creater = StandardDataCreater(SimpleImages(1000, 10, distribution="shifted"))
+    data_creater = StandardDataCreater(SimpleLinear(1000, distribution=2))
     train = data_creater.create_training_loader()
     val = data_creater.create_validation_loader()
     test = data_creater.create_test_loader()
@@ -27,8 +31,11 @@ def fine_tune_model():
     trainer.fit(new_model, train, val)
     trainer.test(new_model, test)
 
-    # print("Pretrained:", old_model.state_dict())
-    # print("Fine Tunes:", new_model.state_dict())
+    if config.model == "simple":
+        print("Pretrained:", old_model.state_dict())
+        print("Fine Tunes:", new_model.state_dict())
+
+    pass
 
 
 if __name__ == "__main__":

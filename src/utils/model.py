beab2452fd8f9db372236275375eb5441a514148
simonrydder <66967123+simonrydder@users.noreply.github.com>
2025-02-17 14:50:27 +0100

    added utils file for model functions (save model structure added)

---
+++ src/utils/model.py
@@ -0,0 +1,29 @@
+import json
+
+from lightning import LightningModule
+
+
+def save_model_structure(
+    model: LightningModule, path: str, include_parameter_count: bool = True
+) -> None:
+    """
+    Saves the structure of a Lightning model's parameters to a JSON file.
+
+    Args:
+        model (LightningModule): The PyTorch Lightning model.
+        path (str): The file path where the JSON structure will be saved.
+
+    Example:
+        save_model_structure(my_model, "model_structure.json")
+    """
+    if include_parameter_count:
+        model_structure = {
+            name: param.numel() for name, param in model.named_parameters()
+        }
+    else:
+        model_structure = {name for name, _ in model.named_parameters()}
+
+    with open(path, "w") as f:
+        json.dump(model_structure, f, indent=4)
+
+    print(f"Model structure saved to {path}")

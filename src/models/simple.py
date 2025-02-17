03227861c4b8b8ae619e32ca8485ecc7429c823c
simonrydder <66967123+simonrydder@users.noreply.github.com>
2025-02-17 10:07:53 +0100

    added training_step, val_step, etc. + types for simple model

--- src/models/simple.py
+++ src/models/simple.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 from lightning import LightningModule
+from torch import Tensor, optim
 
 
 class Encoder(nn.Module): ...
@@ -11,6 +12,36 @@ class Simple(LightningModule):
 
         self.linear = nn.Linear(2, 1)
 
-    def forward(self, x):
+    def forward(self, x: Tensor):
         y = self.linear(x)
         return y
+
+    def training_step(self, batch: tuple[Tensor, Tensor], batch_index: int) -> Tensor:
+        x, y = batch
+        pred = self(x)
+
+        loss = nn.functional.mse_loss(pred, y)
+        self.log("train_loss", loss, prog_bar=True)
+
+        return loss
+
+    def validation_step(self, batch: tuple[Tensor, Tensor], batch_index: int) -> Tensor:
+        x, y = batch
+        pred = self(x)
+
+        loss = nn.functional.mse_loss(pred, y)
+        self.log("val_loss", loss, prog_bar=True)
+
+        return loss
+
+    def test_step(self, batch: tuple[Tensor, Tensor], batch_index: int) -> Tensor:
+        x, y = batch
+        pred = self(x)
+
+        loss = nn.functional.mse_loss(pred, y)
+        self.log("test_loss", loss, prog_bar=True)
+
+        return loss
+
+    def configure_optimizers(self) -> optim.Optimizer:
+        return optim.SGD(self.parameters(), lr=0.01)

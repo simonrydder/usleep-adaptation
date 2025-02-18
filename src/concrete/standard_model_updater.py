21b48b57bd36654fa001b1084bad92b6ad7f970a
simonrydder <66967123+simonrydder@users.noreply.github.com>
2025-02-18 10:05:43 +0100

    freeze all parameters as default for new model

--- src/concrete/standard_model_updater.py
+++ src/concrete/standard_model_updater.py
@@ -13,6 +13,14 @@ class StandardModelUpdater(ModelUpdater):
 
     def adapt(self, model: LightningModule, **kwargs) -> LightningModule:
         new_model = deepcopy(model)
+        new_model = self._freeze_all_parameters(new_model)
+
         self.adapter.adapt(new_model)
 
         return new_model
+
+    def _freeze_all_parameters(self, model: LightningModule) -> LightningModule:
+        for param in model.parameters():
+            param.requires_grad = False
+
+        return model

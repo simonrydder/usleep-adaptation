9fb61945aeb71b31d13a87f1173efd09a3afb88d
simonrydder <66967123+simonrydder@users.noreply.github.com>
2025-02-21 12:35:44 +0100

    updated main with ne wway of creaing adapter

--- main.py
+++ main.py
@@ -4,22 +4,24 @@ from src.concrete.standard_model_trainer import StandardModelTrainer
 from src.concrete.standard_model_updater import StandardModelUpdater
 from src.config.config import load_config
 from src.dataset.resnet.simple_images import SimpleImages
-from src.dataset.simple.simple_linear import SimpleLinear
+from src.utils.model import summarize_lightning
 
 
 def fine_tune_model():
-    config = load_config("simple_bitfit")
+    config = load_config("resnet_conv_adapter")
 
     model_cls = config.get_model_class()
     loader = StandardModelLoader(model_cls)
     old_model = loader.load_pretrained(config.ckpt)
 
-    adapter = config.get_adapter()
+    adapter = config.adapter.get_adapter()
     updater = StandardModelUpdater(adapter)
     new_model = updater.adapt(old_model)
 
+    print(summarize_lightning(new_model, 2))
+
     data_creater = StandardDataCreater(SimpleImages(1000, 10, distribution="shifted"))
-    data_creater = StandardDataCreater(SimpleLinear(1000, distribution=2))
+    # data_creater = StandardDataCreater(SimpleLinear(1000, distribution=2))
     train = data_creater.create_training_loader()
     val = data_creater.create_validation_loader()
     test = data_creater.create_test_loader()
@@ -34,8 +36,6 @@ def fine_tune_model():
     if config.model == "simple":
         print("Pretrained:", old_model.state_dict())
         print("Fine Tunes:", new_model.state_dict())
-
-    pass
 
 
 if __name__ == "__main__":

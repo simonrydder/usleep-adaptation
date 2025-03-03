5cefd527405ea88cfa417c0ac98c82c6260cd4eb
simonrydder <66967123+simonrydder@users.noreply.github.com>
2025-03-03 12:54:18 +0100

    change default patience in early stopping to 25 (lr scheduler is 20)

--- src/config/_early_stopping_setting.py
+++ src/config/_early_stopping_setting.py
@@ -5,6 +5,6 @@ from pydantic import BaseModel
 
 class EarlyStoppingSetting(BaseModel):
     monitor: str
-    patience: int = 5
+    patience: int = 25
     mode: Literal["min", "max"]
     min_delta: float = 0.0001

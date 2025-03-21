2c3d205619f0e03f292c8515a88b63f9907de61e
EskildAndersen <202007483@post.au.dk>
2025-03-21 10:19:43 +0100

    clean up and neptune

--- src/concrete/strategies/adapter_methods/lora.py
+++ src/concrete/strategies/adapter_methods/lora.py
@@ -1,76 +1,36 @@
-import torch.nn as nn
+import loralib as loralib
+import torch
 from lightning import LightningModule
+from peft import LoraConfig, get_peft_model
 
-import src.concrete.strategies.adapter_methods.lora_base as lora
 from src.interfaces.strategies.adapter_method import AdapterMethod
 
 
 class LoRA(AdapterMethod):
-    def __init__(self, rank: int = 1, alpha: int = 1, dropout: float = 0.0) -> None:
+    def __init__(self, rank: int = 1, alpha: float = 1.0, dropout: float = 0.0) -> None:
         self.rank = rank
         self.alpha = alpha
         self.dropout = dropout
         super().__init__()
 
     def apply(self, model: LightningModule) -> LightningModule:
-        for name, child_module in model.named_children():
-            setattr(model, name, self.recursive_apply(child_module))
+        target_modules = []
+        for name, module in model.named_modules():
+            if isinstance(module, torch.nn.Conv1d):
+                target_modules.append(name)
 
-        return model
+            elif isinstance(module, torch.nn.Linear):
+                target_modules.append(name)
 
-    def recursive_apply(self, parent: nn.Module | LightningModule) -> nn.Module:
-        if isinstance(parent, nn.Linear):
-            lora_linear = self.create_new_module(parent)
-            return lora_linear
+            elif isinstance(module, torch.nn.Conv2d):
+                target_modules.append(name)
 
-        if isinstance(parent, nn.Conv1d):
-            lora_conv1d = self.create_new_module(parent)
-            return lora_conv1d
-
-        if isinstance(parent, nn.Conv2d):
-            lora_conv2d = self.create_new_module(parent)
-            return lora_conv2d
-
-        for name, child in parent.named_children():
-            setattr(parent, name, self.recursive_apply(child))
-
-        return parent
-
-    def create_new_module(self, module: nn.Linear | nn.Conv1d | nn.Conv2d):
-        if isinstance(module, nn.Conv1d):
-            return lora.Conv1d(
-                in_channels=module.in_channels,
-                out_channels=module.out_channels,
-                kernel_size=module.kernel_size[0],
-                stride=module.stride,
-                dilation=module.dilation,
-                padding=module.padding,
-                bias=True if module.bias is not None else False,
-                groups=module.groups,
-                r=self.rank,
-                lora_alpha=self.alpha,
-                lora_dropout=self.dropout,
-            )
-
-        elif isinstance(module, nn.Conv2d):
-            return lora.Conv2d(
-                in_channels=module.in_channels,
-                out_channels=module.out_channels,
-                kernel_size=module.kernel_size[0],
-                stride=module.stride,
-                dilation=module.dilation,
-                padding=module.padding,
-                bias=True if module.bias is not None else False,
-                groups=module.groups,
-                r=self.rank,
-                lora_alpha=self.alpha,
-                lora_dropout=self.dropout,
-            )
-        return lora.Linear(
-            in_features=module.in_features,
-            out_features=module.out_features,
-            bias=True if module.bias is not None else False,
+        lora_conf = LoraConfig(
             r=self.rank,
             lora_alpha=self.alpha,
             lora_dropout=self.dropout,
+            target_modules=target_modules,
         )
+        peft_model = get_peft_model(model, lora_conf)
+
+        return peft_model

3ea4e764479c210885e4ce68a7940a486e6404db
simonrydder <66967123+simonrydder@users.noreply.github.com>
2025-04-22 11:29:12 +0200

    added kappa vs. method plot (including original)

--- src/plot/plot_generator.py
+++ src/plot/plot_generator.py
@@ -4,6 +4,7 @@ from src.plot.delta_kappa_vs_methods import (
 from src.plot.delta_kappa_vs_parameters import (
     plot_delta_kappa_vs_parameters,
 )
+from src.plot.kappa_vs_methods import plot_kappa_vs_methods
 from src.plot.pretrained_kappa import pretrained_kappa_performance_plot
 from src.plot.val_kappa import (
     plot_validation_kappa,
@@ -16,6 +17,7 @@ def generate_plots(data: list[MethodData]) -> None:
     plot_validation_kappa(data, "eesm19")
     plot_delta_kappa_vs_methods(data, "eesm19")
     plot_delta_kappa_vs_parameters(data, "eesm19")
+    plot_kappa_vs_methods(data, "eesm19")
 
     pretrained_kappa_performance_plot(data)
 
@@ -24,7 +26,7 @@ if __name__ == "__main__":
     use_downloaded = True
 
     datasets = ["eesm19"]
-    ids = [4]
+    ids = [3]
     methods = None
 
     if use_downloaded:

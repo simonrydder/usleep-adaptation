3692f5a85f87c3ccea3c5c5e0da8acf6e9f1f92d
simonrydder <66967123+simonrydder@users.noreply.github.com>
2025-04-22 11:13:32 +0200

    updated generate_plot function

--- src/plot/plot_generator.py
+++ src/plot/plot_generator.py
@@ -4,10 +4,11 @@ from src.plot.delta_kappa_vs_methods import (
 from src.plot.delta_kappa_vs_parameters import (
     plot_delta_kappa_vs_parameters,
 )
+from src.plot.pretrained_kappa import pretrained_kappa_performance_plot
 from src.plot.val_kappa import (
     plot_validation_kappa,
 )
-from src.utils.neptune_api.data_loader import load_data
+from src.utils.neptune_api.data_loader import get_data, load_data
 from src.utils.neptune_api.method_data import MethodData
 
 
@@ -16,11 +17,20 @@ def generate_plots(data: list[MethodData]) -> None:
     plot_delta_kappa_vs_methods(data, "eesm19")
     plot_delta_kappa_vs_parameters(data, "eesm19")
 
+    pretrained_kappa_performance_plot(data)
+
 
 if __name__ == "__main__":
-    data = load_data(
-        datasets=["eesm19"], ids=[2]
-    )  # Load saved data from results folder
-    # data = get_data("eesm19")  # Get data from neptune
+    use_downloaded = True
+
+    datasets = ["eesm19"]
+    ids = [4]
+    methods = None
+
+    if use_downloaded:
+        data = load_data(datasets, methods, ids)
+    else:
+        data = get_data(datasets, methods, ids)
+
     generate_plots(data)
     pass

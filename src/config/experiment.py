ea63c1a6247a290160dd13c6005da8433a33c4c0
simonrydder <66967123+simonrydder@users.noreply.github.com>
2025-04-27 08:48:30 +0200

    changed name to be similar to yaml file

--- src/config/experiment.py
+++ src/config/experiment.py
@@ -1,4 +1,10 @@
+import itertools
+import os
+
+import yaml
 from pydantic import BaseModel
+
+_YAML_FOLDER = os.path.join("src", "config", "yaml")
 
 
 class Experiment(BaseModel):
@@ -11,7 +17,49 @@ class Experiment(BaseModel):
 
 
 def get_experiment_name(experiment: Experiment) -> str:
-    return experiment.trainer
+    return "_".join([experiment.dataset, experiment.method])
 
 
-def generate_experiments(datasets: list[str]): ...
+def generate_experiments(
+    datasets: list[str],
+    methods: list[str] | None,
+    id: int,
+    seed: int = 42,
+) -> list[Experiment]:
+    if methods is None:
+        methods = _get_yaml_methods()
+
+    exps = []
+    for dataset, method in itertools.product(datasets, methods):
+        exp = Experiment(
+            dataset=dataset,
+            method=method,
+            model="usleep",
+            trainer="usleep",
+            id=id,
+            seed=seed,
+        )
+
+        exps.append(exp)
+
+    return exps
+
+
+def _get_yaml_methods() -> list[str]:
+    global _YAML_FOLDER
+
+    files = os.listdir(os.path.join(_YAML_FOLDER, "adapter_method"))
+    methods = [f.split(".")[0] for f in files]
+
+    methods.remove("_default")
+
+    return methods
+
+
+def save_experiment(experiment: Experiment) -> None:
+    global _YAML_FOLDER
+    name = get_experiment_name(experiment)
+    filename = f"{name}.yaml"
+
+    with open(os.path.join(_YAML_FOLDER, "experiments", filename), "w") as f:
+        yaml.dump(experiment.model_dump(), f)

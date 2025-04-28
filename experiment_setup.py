b7dfd3adf9a2e6b85c4bf226d155a66123f88b85
simonrydder <66967123+simonrydder@users.noreply.github.com>
2025-04-28 12:30:24 +0200

    setup file for experiments

--- experiment_setup.py
+++ experiment_setup.py
@@ -3,7 +3,7 @@ from src.config.experiment import generate_experiments, save_experiment
 if __name__ == "__main__":
     datasets = ["eesm19"]  # Datasets to generate experiments for.
     methods = None  # Methods to generate experiments for (None = 'all').
-    id = 1
+    id = 2
 
     for exp in generate_experiments(datasets, methods, id):
         save_experiment(exp)

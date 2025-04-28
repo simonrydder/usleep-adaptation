5f6d5d486152150e012f76676e143f50c3ad5011
simonrydder <66967123+simonrydder@users.noreply.github.com>
2025-04-28 14:48:34 +0200

    id 99 = test different lr

--- experiment_setup.py
+++ experiment_setup.py
@@ -2,8 +2,18 @@ from src.config.experiment import generate_experiments, save_experiment
 
 if __name__ == "__main__":
     datasets = ["eesm19"]  # Datasets to generate experiments for.
-    methods = None  # Methods to generate experiments for (None = 'all').
-    id = 2
+    methods = [
+        "Full_3",
+        "Full_4",
+        "Full_6",
+        "LoRA20_3",
+        "LoRA20_4",
+        "LoRA20_6",
+        "SCL20_3",
+        "SCL20_4",
+        "SCL20_6",
+    ]  # Methods to generate experiments for (None = 'all').
+    id = 99
 
     for exp in generate_experiments(datasets, methods, id):
         save_experiment(exp)

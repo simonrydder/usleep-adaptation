3e4a3bba8743f4c9c53db71396e964aed9c06d10
simonrydder <66967123+simonrydder@users.noreply.github.com>
2025-05-05 09:33:28 +0200

    prepared new experiments

--- experiment_setup.py
+++ experiment_setup.py
@@ -1,14 +1,16 @@
 from src.config.experiment import generate_experiments, save_experiment
 
 if __name__ == "__main__":
-    datasets = ["eesm19"]  # Datasets to generate experiments for.
-    methods = [
-        "Full",
-    ]  # Methods to generate experiments for (None = 'all').
+    datasets = [
+        "eesm19",
+        "isruc_sg2",
+        "isruc_sg3",
+    ]  # Datasets to generate experiments for.
+    methods = None  # Methods to generate experiments for (None = 'all').
     train_size = None
     folds = None
     seed = 42
-    key = "Eo974696"
+    key = None
 
     for exp in generate_experiments(
         datasets=datasets,

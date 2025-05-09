4a8c8db16d76ac3737db0cbd1a9dae447ea4af3e
simonrydder <66967123+simonrydder@users.noreply.github.com>
2025-05-09 08:38:37 +0200

    hdf5_printer

---
+++ src/utils/hdf5_printer.py
@@ -0,0 +1,17 @@
+import h5py
+
+
+def print_hdf5_structure(file_path: str) -> None:
+    def print_attrs(name, obj):
+        indent = "  " * name.count("/")
+        if isinstance(obj, h5py.Group):
+            print(f"{indent}[Group] {name}")
+        elif isinstance(obj, h5py.Dataset):
+            print(f"{indent}[Dataset] {name} - shape: {obj.shape}, dtype: {obj.dtype}")
+
+    with h5py.File(file_path, "r") as f:
+        f.visititems(print_attrs)
+
+
+if __name__ == "__main__":
+    print_hdf5_structure("data/hdf5/svuh (2).hdf5")

628efe7aa49fa776bb699a0263beb6ca19f89a58
simonrydder <66967123+simonrydder@users.noreply.github.com>
2025-05-28 11:11:07 +0200

    included decimals

--- src/plot/latex_table_formatter.py
+++ src/plot/latex_table_formatter.py
@@ -4,19 +4,20 @@ import pandas as pd
 def highlight_and_color_cell(df: pd.DataFrame) -> pd.DataFrame:
     for col in df.columns[1:]:
         col_vals = df[col]
-        min_val = int(round(col_vals.min(), 0))
+        min_val = col_vals.min()
         sorted_vals = col_vals.sort_values().to_list()
         # median_val = col_vals.median()
 
         def format_cell(val):
-            val_int = int(round(val, 0))
+            # val_int = int(round(val, 0))
+            print_val = round(val, 1)
             color = "green!15" if val in sorted_vals[:10] else "red!15"
             # color = "green!15" if val <= median_val else "red!15"
             cell = rf"\cellcolor{{{color}}}"
-            if val_int == min_val:
-                return cell + rf"\underline{{\textbf{{{val_int}}}}}"
+            if val == min_val:
+                return cell + rf"\underline{{\textbf{{{print_val}}}}}"
             else:
-                return cell + str(val_int)
+                return cell + str(print_val)
 
         df[col] = col_vals.apply(format_cell)
 
